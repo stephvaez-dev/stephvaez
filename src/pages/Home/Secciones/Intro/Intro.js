607b0b0142d512ba5d59110a1d86f29e93bf1d10
tomasDev1995 <tomas.e.aguilera95@gmail.com>
2023-09-03 19:56:41 -0300

    Ajustando resoluciones

--- src/pages/Home/Secciones/Intro/Intro.js
+++ src/pages/Home/Secciones/Intro/Intro.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Intro.scss';
-import introImage from '../../../../assets/images/photo-intro.jpeg';
-import introImage from '../../../../assets/images/img_intro_mobile.png';
+import introImageDesktop from '../../../../assets/images/photo-intro.jpeg';
+import introImageMobile from '../../../../assets/images/img_intro_mobile.png';
 
 const Intro = () => {
   return (
     <section>
-      <img className='imagen-intro' src={introImage} alt="Imagen de introducción" />
+      <img className='imagen-intro' src={introImageDesktop} alt="Imagen de introducción" />
     </section>
   );
 }
