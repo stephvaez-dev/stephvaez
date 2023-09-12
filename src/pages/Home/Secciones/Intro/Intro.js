c14aeb7ca1d335651bced5df315393771e44eb0b
tomasDev1995 <tomas.e.aguilera95@gmail.com>
2023-09-12 02:39:01 -0300

    Se cambia la imagen de la intro, se agrega titulo, descripcion y boton de coleccion

--- src/pages/Home/Secciones/Intro/Intro.js
+++ src/pages/Home/Secciones/Intro/Intro.js
@@ -3,17 +3,16 @@ import './Intro.scss';
 import introImageDesktop from '../../../../assets/images/photo-intro.jpeg';
 import introImageDosDesktop from '../../../../assets/images/imagen-intro-dos.JPG'
 import introImageTresDesktop from '../../../../assets/images/ro_steff.JPG'
-import introImageMobile from '../../../../assets/images/img_intro_mobile.png';
+import introImageMobile from '../../../../assets/images/intro_mobile.png';
 
 const Intro = () => {
   return (
-    <section>
-      <img className='imagen-intro' src={introImageDesktop} alt="Imagen de introducción" />
-      <div className='imagen-intro-dos-box'>
-        <img className='imagen-intro-dos' src={introImageDosDesktop} alt="Imagen de introducción" />
-      </div>
-      <div className='imagen-intro-tres-box'>
-        <img className='imagen-intro-tres' src={introImageTresDesktop} alt="Imagen de introducción" />
+    <section className='intro-container'>
+      <img className='imagen-intro' src={introImageMobile} alt="Imagen de introducción" />
+      <div className='intro-content'>
+        <h1 className='intro-title'>La moda urbana que estabas buscando</h1>
+        <p className='intro-description'>Descubre la fusion perfecta entre diseño y comodidad. Viste la energia que se siente, muestra el estilo que se ve.</p>
+        <button className='intro-button'>EXPLORAR COLECCION</button>
       </div>
     </section>
   );
