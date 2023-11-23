e190ec71dfc68fb25cba71e3e72b11332539604b
stephvaez-dev <stephvaez.develop@gmail.com>
2023-11-23 00:16:14 -0300

    Se ajustan estilos en resolucion mobile

--- src/pages/Home/Secciones/Intro/Intro.js
+++ src/pages/Home/Secciones/Intro/Intro.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Intro.scss';
-import introImageMobile from '../../../../assets/images/intro_mobile.png';
 import Header from '../../../../components/header/Header';
 
 const Intro = () => {
@@ -8,7 +7,6 @@ const Intro = () => {
   return (
     <div id='intro' className='intro-container'>
       <Header />
-      <img className='imagen-intro' src={introImageMobile} alt="Imagen de introducción" />
       <div className='intro-content'>
         <span className='intro-title'>La moda urbana que estabas buscando</span>
         <span className='intro-description'>Descubre la fusion perfecta entre diseño y comodidad. Viste la energia que se siente, muestra el estilo que se ve.</span>
